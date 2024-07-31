de44ed95d63fa38fbc868b6afefb3017f311c5e4
yujian1904 <yujian1904@outlook.com>
2024-07-31 22:15:47 +0800

    20240731

---
+++ Linear/LinkList/LinkList.c
@@ -0,0 +1,31 @@
+#include "LinkList.h"
+#include <stdlib.h>
+
+Status InitList_L(LinkList L)
+{
+    L = (LinkList)malloc(sizeof(Lnode));
+    L->next = NULL;
+    return OK;
+}
+int ListEmpty(const LinkList L)
+{ // 空表返回1否则返回0
+    if (L->next)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+Status DestroyList_L(LinkList L)
+{
+    Lnode *p;
+    while (L)
+    {
+        p = L;
+        L = L->next;
+        free(p)
+    }
+    return OK;
+}
---
+++ Linear/LinkList/LinkList.h
@@ -0,0 +1,29 @@
+// 带头节点的单链表
+#ifndef LINKLIST_H_
+#define LINKLIST_H_
+enum State
+{
+    INFEASIBLE = -1,
+    OVERFLOW = -2,
+    ERROR = 0,
+    False = 0,
+    OK = 1,
+    True = 1
+};
+typedef int Status;
+
+typedef int ElemType;
+
+typedef struct Lnode
+{
+    ElemType data;
+    struct Lnode *next;
+} Lnode, *LinkList; // Lnode表示节点，LinkList表示链表亦头结点
+
+Status InitList_L(LinkList *L);
+
+int ListEmpty(const LinkList L);
+
+Status DestroyList_L(LinkList L);
+
+#endif
